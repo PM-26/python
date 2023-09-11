eb6f2e289872394df03ac0d385ed6a4a3bada0f2
PM <107318440+PM-26@users.noreply.github.com>
2023-09-11 06:58:39 +0000

    Done.

---
+++ conditional.py
@@ -0,0 +1,92 @@
+# WAP to find the multiples of a number out of given 5 numbers.
+# WAP to find multiple of numbers taken as input
+# WAP to test the divisibility of a number with another number.
+# WAP that reads two numbers and arithmetic operator and displays the compound result.
+# WAP that reads 3 number and arrange them in ascending and descending order.
+
+# num1=int(input("Enter first number:"))
+# num2=int(input("Enter second number:"))
+# num3=int(input("Enter third number:"))
+# num4=int(input("Enter fourth number:"))
+# num5=int(input("Enter fifth number:"))
+# multiple=int(input("Enter the number to check its multiple from given number:"))
+
+# print("The multiples are:")
+
+# if(num1%multiple==0):
+#     print(num1)
+# if(num2%multiple==0):
+#     print(num2)
+# if(num3%multiple==0):
+#     print(num3)
+# if(num4%multiple==0):
+#     print(num4)
+# if(num5%multiple==0):
+#     print(num5)
+
+# for i in range(5):
+#     num=int(input("Enter a number: "))
+#     print("The multiples are:")
+#     for i in range(1,11):
+#         print(num*i,end=" ")
+#     print("\n")
+
+# num1=int(input("Enter first number: "))
+# num2=int(input("Enter second number: "))
+# if (num1%num2==0):
+#     print("Yes, the number is divisible.")
+# else:
+#     print("No, the number is not divisible.")
+
+# num1=int(input("Enter first number: "))
+# num2=int(input("Enter second number: "))
+# operator=input("Enter operator: ")
+# if(operator=="+"):
+#     print("Result =",num1+num2)
+# elif(operator=="-"):
+#     print("Result =",num1-num2)
+# elif(operator=="*"):
+#     print("Result =",num1*num2)
+# elif(operator=="/"):
+#     print("Result =",num1/num2)
+# elif(operator=="**"):
+#     print("Result =",num1**num2)
+
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+# c=int(input("Enter third number: "))
+
+# print("Descending Order: ",end="")
+
+# if(a>b and a>c):
+#     if(b>c):
+#         print(a,b,c)
+#     elif(c>b):
+#         print(a,c,b)
+# elif(b>a and b>c):
+#     if(a>c):
+#         print(b,a,c)
+#     elif(c>a):
+#         print(b,c,a)
+# elif(c>a and c>b):
+#     if(b>a):
+#         print(c,b,a)
+#     elif(a>b):
+#         print(c,a,b)
+
+# print("Ascending Order: ",end="")
+# if (a<b and a<c):
+#     if (b<c):
+#         print(a,b,c)
+#     else:
+#         print(a,c,b)
+# elif(b<a and b<c):
+#     if(a<c):
+#         print(b,a,c)
+#     else:
+#         print(b,c,a)
+# elif(c<a and c<b):
+#     if(a<b):
+#         print(c,a,b)
+#     else:
+#         print(c,b,a)
