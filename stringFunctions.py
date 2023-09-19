75c6d5019437367a1888a823b86517406060bd70
PM <107318440+PM-26@users.noreply.github.com>
2023-09-19 07:59:02 +0000

    stringFunctions.py

---
+++ stringFunctions.py
@@ -0,0 +1,14 @@
+string = "Hello World"
+
+print("String:\"Hello World\"")
+print("Converted into uppercase:", string.upper())
+print("Check if it is in uppercase.:", string.isupper())
+print("Converted into lowercase:", string.lower())
+print("Check if its in lowercase.:", string.islower())
+print("Check if it contains space.:", string.isspace())
+print("Length of string:", len(string))
+print("Check if its alphabetic:", string.isalpha())
+print("Check if it contains only digits:", string.isdigit())
+print("Converted into title case:", string.title())
+print("Capitalized:", string.capitalize())
+print("Replace World with India:", string.replace("World", "India"))
