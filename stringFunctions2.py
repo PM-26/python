6b7dd7cf74cdb50e56e866ecb6d65e250c77fe92
PM <107318440+PM-26@users.noreply.github.com>
2023-09-25 04:23:33 +0000

    remaining string functions

---
+++ stringFunctions2.py
@@ -0,0 +1,14 @@
+string=" Hello World! "
+print("String= \" Hello World! \"")
+print("lstrip()=",string.lstrip())
+print("rstrip()=",string.rstrip())
+print("swapcase()=",string.swapcase())
+print("endswith()=",string.endswith(" "))
+print("startswith()=",string.startswith(" "))
+print("find()=",string.find("Hello"))
+print("split()=",string.split())
+
+
+
+
+
