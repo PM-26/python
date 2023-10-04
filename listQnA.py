9f38bd3fc137f68ed80f6dcdd400376c7fc882fb
PM <107318440+PM-26@users.noreply.github.com>
2023-10-04 05:31:37 +0000

    listQnA

---
+++ listQnA.py
@@ -0,0 +1,8 @@
+# Q Write a program to find smallest and largest number in a list.
+
+list=[1,2,3,4];
+
+print(f"List -> {(list)}");
+print(f"Largest Number in string -> {max(list)}");
+print(f"Smallest Number in string -> {min(list)}");
+
