c0e11a4355bfd73de869a04e478a8b9ec48eee12
PM <107318440+PM-26@users.noreply.github.com>
2023-10-10 15:59:19 +0000

    emailScanner

---
+++ emailScanner.py
@@ -0,0 +1,12 @@
+# scan email 
+# form a tuple 
+# user name 
+# domain
+
+email=input("Enter an email->")
+split=email.find("@")
+username=email[:split]
+domain=email[split:]
+tuple=(username,domain)
+print(tuple)
+
